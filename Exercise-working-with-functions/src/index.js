e07e1a288612b65db17c93927c58fff7287c9f0a
renukateppala <142851316+renukateppala@users.noreply.github.com>
2023-09-02 21:17:33 -0400

    Code Exercise: Working with Functions
    Exercise: Working with Functions
    Your task is to write a new function that should be named combine and have the following characteristics:
    
    Accept three input values
    
    Calculate a new value based on the three input values: a * b / c (if a, b & c are the input values)
    
    Return the calculated result

---
+++ Exercise-working-with-functions/src/index.js
@@ -0,0 +1,12 @@
+function combine(a, b, c) {
+  // Calculate the result based on the formula: a * b / c
+  var result = (a * b) / c;
+  return result;
+}
+
+// Example usage:
+var a = 5;
+var b = 10;
+var c = 2;
+var calculationResult = combine(a, b, c);
+console.log("The calculated result is: " + calculationResult);
